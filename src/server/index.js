b38394b408226a6c9aeb96212853f0f712a8b172
Håkan Johansson <hakan@johanssonit.se>
2018-10-07 19:44:08 +0200

    fe

--- src/server/index.js
+++ src/server/index.js
@@ -10,8 +10,10 @@ const app = express();
 
 app.use(express.static("dist"));
 
+const html = renderToString(sheet.collectStyles(<App />));
+const styleTags = sheet.getStyleTags(); // or sheet.getStyleElement();
+
 app.get("*", (req, res) => {
-  const html = renderToString(sheet.collectStyles(<App />));
 
   res.send(`
       <!DOCTYPE html>
@@ -20,6 +22,7 @@ app.get("*", (req, res) => {
         <meta name="keywords" content="YouTube to mp3, mp3 converter, YouTube mp3, YouTube to mp3 converter, convert YouTube to mp3, video to mp3, download YouTube mp3, mp3 download, mp3converter"/>
   	    <meta name="description" content="Convert YouTube video clips to high quality MP3 files in seconds and play them on your preferred MP3 player."/>
   	    <meta name="robots" content="index, follow">
+        ${styleTags}
       </head>
       <body>
         <div id="root">${html}</div>
